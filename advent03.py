af5ff54f9e77367ee2af9c07309718d4f33b1a3b
jolitti <marcodeltrionfo@gmail.com>
2021-12-03 20:52:47 +0100

    Solved part 2 of problem 3

--- advent03.py
+++ advent03.py
@@ -1,10 +1,51 @@
-import string
+def nukeList(l:list[str],filterValue:str,index) -> list[str]:
+    newL = [x for x in l if x[index]==filterValue]
+    return newL
+
+def getOx(_data:list[str]) -> int:
+    v = _data[:]
+    numLen = len(v[0])
+    for j in range(numLen):
+        if len(v)==1: break
+        ones, zeros = 0,0
+        
+        for n in v:
+            if n[j]=="1":ones+=1
+            else: zeros += 1
+    
+        if ones>=zeros: v = nukeList(v,"1",j)
+        else: v = nukeList(v,"0",j)
+    
+    return int(v[0],2)
+
+def getCO2(_data:list[str]) -> int:
+    v = _data[:]
+    numLen = len(v[0])
+    for j in range(numLen):
+        if len(v)==1: break
+        ones, zeros = 0,0
+        
+        for n in v:
+            if n[j]=="1":ones+=1
+            else: zeros += 1
+    
+        if ones>=zeros: v = nukeList(v,"0",j)
+        else: v = nukeList(v,"1",j)
+    
+    return int(v[0],2)
+
 
 
 with open("input03.txt") as file:
     data = file.readlines()
 
 data = [l.strip() for l in data]
+
+x= getOx(data)
+y= getCO2(data)
+print(x)
+print(y)
+print(x*y)
 
 """ gamma = ""
 
