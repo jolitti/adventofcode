850e1b495c399eaaa7dc7c7f4cd0d7febede40fe
jolitti <marcodeltrionfo@gmail.com>
2021-12-05 21:51:28 +0100

    Almost completed framework for answer for day 5

--- 05/advent05.py
+++ 05/advent05.py
@@ -1,4 +1,25 @@
 import re
+from math import copysign
+
+def checkOrto(t:tuple) -> bool:
+    x,y = t
+    a,b,c,d = x[0],x[1],y[0],y[1]
+    return a==c or b==d
+
+def getLine(t:tuple) -> list[tuple[int,int]]:
+    v,w = t
+    a,b,c,d = v[0],v[1],w[0],w[1]
+    dx = int(copysign(1,c-a)) if c-a != 0 else 0
+    dy = int(copysign(1,d-b)) if d-b != 0 else 0
+    x,y = a,b
+    ans = [(x,y)]
+    while x!=c or y!=d:
+        x+=dx
+        y+=dy
+        ans.append((x,y))
+    return ans
+
+print(getLine(((0,0),(5,0))))
 
 with open("05/input00.txt") as file:
     data = file.readlines()
@@ -7,6 +28,7 @@ points = []
 for d in data:
     a,b,c,d = map(int,re.split(",| -> ",d))
     points.append(((a,b),(c,d)))
-print(len(points))
+points = [x for x in points if checkOrto(x)] # Restrict to only ortogonal ones
+# print(len(points))
 
 vents = {}
