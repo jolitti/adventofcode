d0f283c881eb4b2384cdae515140b0f4ceb06f49
jolitti <marcodeltrionfo@gmail.com>
2022-01-11 14:27:23 +0100

    Removed duplicate folders

--- 11/advent11.py
+++
@@ -1,8 +0,0 @@
-from octoboard import OctoBoard
-
-with open("11/input11.txt") as file:
-    data = file.readlines()
-data = [d.strip() for d in data]
-
-b = OctoBoard(data)
-print(b.iterateTilAllFlash() + 1)
