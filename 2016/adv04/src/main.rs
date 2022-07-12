f8745dbafc79b29e277a342efaefeff37fdd7fcb
jolitti <marcodeltrionfo@gmail.com>
2022-07-12 22:07:40 +0200

    Solved 2016 day 4 part 2

--- 2016/adv04/src/main.rs
+++ 2016/adv04/src/main.rs
@@ -1,12 +1,24 @@
-// 409147
+// 409147 991
 
 mod room;
 use room::Room;
 
 const INPUT: &str = include_str!("input.txt");
+const TARGET_ROOM: &str = "northpole object storage ";
 
 fn main() {
 
     let total: i32 = INPUT.lines().map(|l| Room::new(l).value()).sum();
     println!("First answer: {total}");
+
+    let real_rooms = INPUT.lines()
+                    .map(|l| Room::new(l))
+                    .filter(|r| r.is_valid())
+                    .collect::<Vec<_>>();
+
+    for r in real_rooms {
+        if r.real_name() == TARGET_ROOM {
+            println!("{}{} {}",r.real_letters,r.code,r.real_name());
+        }
+    }
 }
--- 2016/adv04/src/room.rs
+++ 2016/adv04/src/room.rs
@@ -3,13 +3,18 @@ use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct Room {
-    letters: String,
+    pub real_letters: String,
+    pub letters: String,
     checksum: String,
-    code: i32
+    pub code: i32
 }
 
 impl Room {
     pub fn new(s:&str) -> Self {
+        let real_letters = String::from(
+            Regex::new(r"[a-z\-]*").unwrap().find(s).unwrap().as_str()
+        );
+
         let letters = s.chars()
                     .take_while(|c| !c.is_numeric())
                     .filter(|c| c.is_alphabetic())
@@ -24,13 +29,14 @@ impl Room {
         let code = number_regex.find(s).unwrap().as_str().parse::<i32>().unwrap();
 
         Room {
+            real_letters,
             letters,
             checksum,
             code
         }
     }
 
-    fn is_valid(self) -> bool {
+    pub fn is_valid(&self) -> bool {
         let mut dict: HashMap<char,i32> = HashMap::new();
         for c in self.letters.chars() {
             let num = dict.get(&c).unwrap_or(&0);
@@ -44,12 +50,25 @@ impl Room {
         first_five == self.checksum
     }
 
-    pub fn value(self) -> i32 {
+    pub fn value(&self) -> i32 {
         let code = self.code;
         match self.is_valid() {
             true => code,
             false => 0
         }
+    }
+
+    pub fn real_name(&self) -> String {
+        let mut ans = String::new();
+        for c in self.real_letters.chars() {
+            ans.push(
+                match c {
+                    '-' => ' ',
+                    _ => shift_letter(c, self.code)
+                }
+            );
+        }
+        ans
     }
 }
 
@@ -63,3 +82,30 @@ fn compare_dict(a:&(&char,&i32),b:&(&char,&i32)) -> std::cmp::Ordering {
     if num_ord != Ordering::Equal { num_ord }
     else { b_c.cmp(a_c) }
 }
+
+const LETTERS: &str = "abcdefghijklmnopqrstuvwxyz";
+
+fn letter_to_num(c:char) -> i32 {
+    let chars_to_letters: HashMap<char,i32> = LETTERS
+                                        .chars()
+                                        .enumerate()
+                                        .map(|(n,c)| (c,n as i32))
+                                        .collect();
+
+    *chars_to_letters.get(&c).unwrap()
+}
+
+fn num_to_letter(n:i32) -> char {
+    let letters_to_chars: HashMap<i32,char> = LETTERS
+                                        .chars()
+                                        .enumerate()
+                                        .map(|(n,c)| (n as i32, c))
+                                        .collect();
+
+    *letters_to_chars.get(&n).unwrap()
+}
+
+pub fn shift_letter(c:char,i:i32) -> char {
+    let x = (letter_to_num(c) + i % LETTERS.len() as i32) % LETTERS.len() as i32;
+    num_to_letter(x)
+} 
