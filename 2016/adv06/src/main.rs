c104e907a7316d269492d9469cad50e95041c514
jolitti <marcodeltrionfo@gmail.com>
2022-07-27 17:33:26 +0200

    Solved 2016 day 6

--- 2016/adv06/src/main.rs
+++ 2016/adv06/src/main.rs
@@ -1,5 +1,30 @@
+// asvcbhvg odqnikqv
+
+use std::collections::HashMap;
+
 const INPUT: &str = include_str!("input.txt");
 
 fn main() {
-    println!("Hello, world!");
+    let mut ans = String::new();
+
+    for i in 0..8 {
+        let mut letters: HashMap<char,usize> = HashMap::new();
+
+        for s in INPUT.lines() {
+            let c = s.chars().nth(i).unwrap();
+
+            let n = letters.entry(c).or_insert(0).clone();
+            letters.insert(c, n + 1);
+        }
+
+        let mut letters = letters.iter().collect::<Vec<_>>();
+        
+        // original (most common): letters.sort_by(|a,b| b.1.cmp(a.1));
+        letters.sort_by(|a,b| a.1.cmp(b.1));
+
+        let (c,_) = letters[0];
+
+        ans.push(*c);
+    }
+    println!("{ans}");
 }
