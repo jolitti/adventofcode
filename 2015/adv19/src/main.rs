64f5aba4ca10c68f8ab47edc19e329e9da4e7fe8
jolitti <marcodeltrionfo@gmail.com>
2022-10-17 14:31:46 +0200

    Progress on 2015 day 19

--- 2015/adv19/src/main.rs
+++ 2015/adv19/src/main.rs
@@ -1,3 +1,18 @@
+use std::collections::HashSet;
+
+const DATA: &str = include_str!("input.txt");
+
 fn main() {
-    println!("Hello, world!");
+    let initial_molecule: String = String::from(DATA.lines().last().unwrap());
+    let subs_number = DATA.lines().count() - 1;
+    let substitutions: Vec<(String,String)> = DATA.lines().filter(|l| l.split(" ").count()==3)
+        .map(|l|
+            {
+                let words = l.split(" ").collect::<Vec<_>>();
+                let (a,_,c) = (words[0],words[1],words[2]);
+                return (String::from(a),String::from(c));
+            }
+    ).collect::<Vec<_>>();
+
+    println!("{substitutions:?}");
 }
