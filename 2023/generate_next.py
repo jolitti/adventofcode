aca92bba5a9ccd0e80eff92f1f7b5608946b2e25
jolitti <marcodeltrionfo@gmail.com>
2023-12-06 10:31:18 +0100

    generated day 7, changed script to avoid overwriting existing files

--- 2023/generate_next.py
+++ 2023/generate_next.py
@@ -1,6 +1,11 @@
 import sys
+import os
 
 day = sys.argv[1]
+
+if os.path.exists("day"+day+".py"):
+    print("Denied: file already exists")
+    sys.exit()
 
 with open("template.py") as file:
     file_content = file.read()
